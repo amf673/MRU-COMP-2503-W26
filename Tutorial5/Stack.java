3796a42b374192671d59a1d1cd4a441104a3de9e
Alan <afedoruk@mtroyal.ca>
2026-02-07 06:15:10 -0700

    Add files via upload

---
+++ Tutorial5/Stack.java
@@ -0,0 +1,25 @@
+public class Stack<T extends Comparable<T>> extends SLL<T> {
+	// add an element data to the top of the stack.
+	public void push(T data) {
+		// Todo: add an element data to the top of the stack.
+		// use addHead
+	}
+
+
+	public T pop() {
+		// remove an element from the top of the stack.
+		// use deleteHead
+	}
+
+
+	public T peek() {
+		// Todo: return a pointer to the top element on the stack,
+		// without removing it.
+		// use getHead;
+	}
+
+	public boolean isEmpty() {
+		// return true if the stack is empty, false otherwise
+		// which method from SSL should you use?
+	}
+}
