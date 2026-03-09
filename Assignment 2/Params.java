04e2663ac7e0f950f54a57f1b85c584be8e4e888
Alan <afedoruk@mtroyal.ca>
2026-03-09 10:09:11 -0600

    Update Params.java

--- Assignment 2/Params.java
+++ Assignment 2/Params.java
@@ -2,10 +2,12 @@
 public class Params {
 
 	public static void main( String[] args) {
+	/* Shows how to access command line arguments */
 
 		for ( int i = 0; i < args.length; i++) {
 			System.out.println( i + ": " + args[i]); 
 		}
+		// Shows how to split a csv structured string into an array 
 		String line = args[0];
 		System.out.println( line); 
 		String[] columns = line.split(",");
@@ -15,4 +17,5 @@ public class Params {
 		}
 	}
 }
+
 
